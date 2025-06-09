26dd29dde3adf810579a1ddccaba4b7a61879b13
khafmann <khafmann@gmail.com>
2025-06-09 17:36:30 +0500

    Подправил конфиги для запуска

--- tech.support/src/main/java/com/example/tech/support/Application.java
+++ tech.support/src/main/java/com/example/tech/support/Application.java
@@ -3,7 +3,7 @@ package com.example.tech.support;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.tech.support")
 public class Application {
 
 	public static void main(String[] args) {
--- tech.support/src/main/java/com/example/tech/support/model/entity/UserInfo.java
+++ tech.support/src/main/java/com/example/tech/support/model/entity/UserInfo.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-public class User {
+public class UserInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
